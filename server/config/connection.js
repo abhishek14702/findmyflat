52cf7a73c754ef482bd46f2932445e3146061472
abhishek bishnoi <abhishek14702@gmail.com>
2023-11-02 05:02:42 +0530

    fixed authentication

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/YardGems', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://abhi123:DwRcuTran5brUdPh@cluster0.gu2nqeg.mongodb.net/', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
